be5fbe825334001109f95185848f19a00ce0c0f2
dingzu <349923359@qq.com>
2023-07-08 18:50:41 +0800

    更新到 GPT4

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -26,7 +26,8 @@ export const post: APIRoute = async ({ request }) => {
       "Content-Type": `application/json`
     },
     body: JSON.stringify({
-      model: "gpt-3.5-turbo",
+      // model: "gpt-3.5-turbo",
+      model: "gpt-4",
       messages: [
         {
           role: "user",

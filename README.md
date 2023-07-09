9fab6b31d2944f8fbb6352e3a61eeb963e99c39e
dingzu <dzwangyihan@gmail.com>
2023-07-09 10:27:32 +0800

    更新

--- README.md
+++ README.md
@@ -29,7 +29,7 @@
     ```ts
     const defaultSetting = {
         // 连续对话，每次都需要将上下文传给 API，比较费钱，而且同样有 4096 token 的限制
-        continuousDialogue: true,
+        continuousDialogue: false,
         // 记录对话内容，刷新后不会清空对话
         archiveSession: false,
         openaiAPIKey: "",
--- src/default.ts
+++ src/default.ts
@@ -1,13 +1,13 @@
 export const defaultSetting = {
   continuousDialogue: false,
-  archiveSession: true,
+  archiveSession: false,
   openaiAPIKey: "",
   openaiAPITemperature: 60,
   systemRule: ""
 }
 
 export const defaultMessage = `
-  - 由 D\\Z 代理的 CHAT-GPT4 版本
+  - 由 DZ 代理的 CHAT-GPT4 版本
   - 训练数据截止到 2021 年 9 月
   - 由于 GPT4 的 Token 消耗量过大，已默认关闭连续对话，如有需要，在设置中自行打开
 `
